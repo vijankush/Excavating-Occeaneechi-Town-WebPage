0b127f55b33ebefa7078adda0c84af3189c6d7e7
Ankush Vij <45926285+vijankush@users.noreply.github.com>
2020-10-23 18:30:23 -0400

    pop-up and zoom in working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,42 @@ $('[data-toggle="popover-hover"]').popover({
     content: function () { return '<img src="' + $(this).data('img') + '" />'; }
   });
 
-$(function () {
-  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+
+  var zoom_percent = "100";
+  function zoom(zoom_percent){
+      $(".mfp-figure figure").click(function(){
+          switch(zoom_percent){
+              case "100":
+                  zoom_percent = "150";
+                  break;
+              case "120":
+                  zoom_percent = "150";
+                  break;
+              case "150":
+                  zoom_percent = "200";
+                  $(".mfp-figure figure").css("cursor", "zoom-out");
+                  break;
+              case "200":
+                  zoom_percent = "100";
+                  $(".mfp-figure figure").css("cursor", "zoom-in");
+                  break;
+          }
+          $(this).css("zoom", zoom_percent+"%");
+      });
+  }
+
+  $(document).ready(function() {
+    $('.popup-link').magnificPopup({
+      type:'image',
+      callbacks: {
+        open: function() {
+          $(".mfp-figure figure").css("cursor", "zoom-in");
+          zoom(zoom_percent);
+        },
+        close: function() {
+          // Will fire when popup is closed
+        }
+        // e.t.c.
+      }
+    });
   });
