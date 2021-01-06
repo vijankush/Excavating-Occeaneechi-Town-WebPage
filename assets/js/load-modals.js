e3bc3c8a0706f4dd11c45e9cdda501cf20bd2b18
aychen99 <42681629+aychen99@users.noreply.github.com>
2021-01-06 14:05:50 -0500

    Add site generated from code as of Jan 6 2021

--- assets/js/load-modals.js
+++ assets/js/load-modals.js
@@ -1,30 +1,23 @@
-$(document).ready(function() {
-  $("#carousel-zoom").lightGallery({
-      selector: 'a'
-  });
-});
+/* Script that loads table, reference, and video modals while also initializing
+ * a LightGallery for each image.
+ * Requires jQuery due to use of LightGallery.
+ */
 
 function setUpModals() {
   let contents = document.getElementById("text-chapter-contents");
-  let imgAnchors = contents.getElementsByClassName("a-img");
-  let refAnchors = contents.getElementsByClassName("a-ref");
-  let tableAnchors = contents.getElementsByClassName("a-table")
-  let modalBody = document.getElementById("modal-body");
-  let tableImgModalBody = document.getElementById("table-img-modal-body")
-  for (let anchor of imgAnchors) {
-    anchor.onclick = function(e) {
-      modalBody.innerHTML = `
-      <img src="${anchor.getAttribute("data-figure-path")}"></img>
-      <br>
-      <br>
-      ${anchor.getAttribute("data-figure-caption")}`;
-      modalBody.classList.add('img-modal-body');
-      modalBody.classList.remove('ref-modal-body');
-      modalBody.classList.remove('table-modal-body');
-    }
+  if (!contents) {
+    return;
   }
+  let refAnchors = contents.getElementsByClassName("a-ref");
+  let tableAnchors = contents.getElementsByClassName("a-table");
+  let videoAnchors = contents.getElementsByClassName("a-video");
+  let modalBody = document.getElementById("modal-body");
+  let modalDialog = document.getElementById("genModal").getElementsByClassName("modal-dialog")[0];
   for (let anchor of refAnchors) {
     anchor.onclick = function(e) {
+      // Revert style rules applied when opening a table
+      modalDialog.style.width = "";
+      modalDialog.style.maxWidth = "";
       modalBody.innerHTML = `${anchor.getAttribute("data-author")}<br><p>${anchor.getAttribute("data-ref-text")}</p>`;
       modalBody.classList.add('ref-modal-body');
       modalBody.classList.remove('img-modal-body');
@@ -32,23 +25,73 @@ function setUpModals() {
     }
   }
   for (let anchor of tableAnchors) {
+    // Insert table and set it to "display:none;" so LightGallery can work on any image links in a table
+    let hiddenDiv = document.createElement("div");
+    let tablePreContainer = document.createElement("div");
+    tablePreContainer.innerHTML = `<pre>${anchor.getAttribute("data-table-string")}</pre>`;
+    tablePreContainer.classList.add("modal-pre-container-div");
+    hiddenDiv.appendChild(tablePreContainer);
+    hiddenDiv.style.display = "none";
+    document.body.appendChild(hiddenDiv);
     anchor.onclick = function(e) {
-      modalBody.innerHTML = (`<p>${anchor.getAttribute("data-table-header")}</p>`
-                             + `<pre>${anchor.getAttribute("data-table-string")}</pre>`);
-      for (let tableImgAnchor of modalBody.getElementsByTagName("a")) {
-        tableImgAnchor.onclick = function() {
-          tableImgModalBody.innerHTML = `
-          <img src="${tableImgAnchor.getAttribute("data-figure-path")}"></img>
-          <br>
-          <br>
-          ${tableImgAnchor.getAttribute("data-figure-caption")}`;
-        }
-      }
+      // Adjust modal width according to table size
+      modalDialog.style.width = "fit-content";
+      modalDialog.style.maxWidth = "95%";
+      modalBody.innerHTML = "";
+      modalBody.innerHTML = `<p>${anchor.getAttribute("data-table-header")}</p>`;
+      modalBody.appendChild(tablePreContainer);
       modalBody.classList.add('table-modal-body');
       modalBody.classList.remove('img-modal-body');
       modalBody.classList.remove('ref-modal-body');
-    }
+    };
+  }
+  for (let anchor of videoAnchors) {
+    youTubeLink = anchor.getAttribute("data-figure-path");
+    anchor.onclick = function(e) {
+      // Revert style rules applied when opening a table
+      modalDialog.style.width = "";
+      modalDialog.style.maxWidth = "";
+      modalBody.innerHTML = `
+      <div class="embed-responsive embed-responsive-4by3">
+        <iframe class="embed-responsive-item" src="${anchor.getAttribute("data-figure-path")}"
+        webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+      <br>
+      <p class="text-center">${anchor.getAttribute("data-figure-caption")}</p>
+      `;
+    };
   }
 };
 
-setUpModals();
+$(document).ready(function() {
+  // Initialize any carousels (i.e. in Excavation context pages)
+  $("[id^=carousel-zoom]").lightGallery({
+    selector: 'a'
+  });
+  // Initialize any images in Archaeology Primer chapter text
+  $("[id^=archaeology-images]").lightGallery({
+    selector: 'a'
+  });
+  // Initialize images on pages 13, 14, and 23 of the Primer
+  $("#changeable-image-table").lightGallery({
+    selector: 'a'
+  });
+  // Initialize any image links in paragraphs of text
+  $("[class^=a-img]").lightGallery({
+    selector: 'this'
+  });
+
+  setUpModals();
+  // Set up LightGallery for images in tables 3 and 4
+  $("pre").lightGallery({
+    selector: "a",
+    exThumbImage: "data-thumbnail"
+  });
+  // Make LightGallery's opening hide the table modal
+  $("pre").on("onBeforeOpen.lg", function(e) {
+    $("#genModal").css("z-index", 1000);
+  });
+  $("pre").on("onCloseAfter.lg", function(e) {
+    $("#genModal").css("z-index", "");
+  });
+});
